3d8320281a6d9d672f6aff1a4197771b9f0e58db
Atittaya27 <147577320+Atittaya27@users.noreply.github.com>
2025-03-07 11:57:00 +0700

    ย้าย

--- index.js
+++ index.js
@@ -1,4 +1,2 @@
 console.log("น้อยหน่ากินกระดาษ")
-console.log("nannyyyy")
-console.log("Tonmai")
-console.log("Noynha")
+
---
+++ log.js
@@ -0,0 +1,3 @@
+console.log("nannyyyy")
+console.log("Tonmai")
+console.log("Noynha")
